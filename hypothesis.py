b6c812f78f2610100e1d65ac2a68fa63d5ecb2db
Aantoonn <anton.netterstrom@gmail.com>
2026-02-23 11:25:59 +0100

    Hypothesis file

---
+++ hypothesis.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from statistics import variance
+from scipy import stats
+import math
+
+"""
+Two independent sample hypothesis test
+Group 1: Free
+Group 2: Premium
+
+Using variable sessions_per_week
+
+One sided test (Left tail)
+
+Hypothesis: Mi Free < Mi Premium
+
+"""
+
+def hypothesis_test():
+    pass
