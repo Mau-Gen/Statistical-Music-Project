39ddb0600c44a0f2fdaae2ed95ca9038d00d059f
Aantoonn <anton.netterstrom@gmail.com>
2026-02-23 12:50:45 +0100

    Hypothesis test

--- hypothesis.py
+++ hypothesis.py
@@ -11,7 +11,7 @@ Two independent sample hypothesis test
 Group 1: Free
 Group 2: Premium
 
-Using variable sessions_per_week
+Using variable duration_per_week
 
 One sided test (Left tail)
 
@@ -19,5 +19,14 @@ Hypothesis: Mi Free < Mi Premium
 
 """
 
-def hypothesis_test():
-    pass
+def hypothesis_test(conn):
+    free_duration = pd.read_sql("""SELECT user_id, 
+                                SUM(duration) / (MAX(DATE(ld.listened_at)) - MIN(DATE(ld.listened_at)) + 1)  AS duration_week
+                                FROM listening_data ld
+                                JOIN users ON ld.user_id = users.id
+                                WHERE users.subscription = 'free'
+                                GROUP BY user_id""", conn)
+
+    print(free_duration)
+
+    #premium_duration = pd.read_sql("""SELECT user_id, SUM(duration) AS duration_week""", conn)
